b45ec1906e9a5b813374abd319c86de9fe7a723b
Trinh Xuan Tung <xuantung.trinh.63@gmail.com>
2023-12-26 10:01:20 +0700

    use 'with Session.begin() as session'

--- databases/postgresql.py
+++ databases/postgresql.py
@@ -8,6 +8,8 @@ from utils.logger_utils import get_logger
 
 logger = get_logger('PostgreSQL')
 
+Session = sessionmaker()
+
 
 class PostgresDB:
     def __init__(self, connection_url: str = None):
@@ -15,96 +17,88 @@ class PostgresDB:
         if not connection_url:
             connection_url = PostgresDBConfig.CONNECTION_URL
         self.engine = create_engine(connection_url)
-        self.session = sessionmaker(bind=self.engine)()
+        Session.configure(bind=self.engine)
 
-    def close(self):
-        self.session.close()
-
-    def get_all_schemas(self):
-        schemas = self.session.execute("""
-            SELECT schema_name
-            FROM information_schema.schemata;
-        """)
-        self.session.commit()
+    @staticmethod
+    def get_all_schemas():
+        with Session.begin() as session:
+            schemas = session.execute("""
+                SELECT schema_name
+                FROM information_schema.schemata;
+            """)
         return [schema[0] for schema in schemas]
 
-    def get_all_table(self, schema):
-        tables = self.session.execute(f"""
-                        SELECT table_name
-                        FROM information_schema.tables
-                        WHERE table_schema = '{schema}';
-                    """)
+    @staticmethod
+    def get_all_table(schema):
+        with Session.begin() as session:
+            tables = session.execute(f"""
+                            SELECT table_name
+                            FROM information_schema.tables
+                            WHERE table_schema = '{schema}';
+                      """)
         return [table['table_name'] for table in tables]
 
     ###################################
     #            Monitoring           #
     ###################################
-    def get_tables_size(self, schema: str = 'public') -> dict:
-        query = f"""
-            SELECT table_name,
-                   pg_size_pretty(pg_total_relation_size(concat('{schema}.', table_name)))
-            FROM information_schema.tables
-            WHERE table_schema = '{schema}'
-            ORDER by table_name"""
-        cursor = self.session.execute(query)
-        self.session.commit()
+    @staticmethod
+    def get_tables_size(schema: str = 'public') -> dict:
+        with Session.begin() as session:
+            query = f"""
+                SELECT table_name,
+                       pg_size_pretty(pg_total_relation_size(concat('{schema}.', table_name)))
+                FROM information_schema.tables
+                WHERE table_schema = '{schema}'
+                ORDER by table_name"""
+            cursor = session.execute(query)
         return {row['table_name']: row['pg_size_pretty'] for row in cursor}
 
     ###################################
     #      Wallet Address Table       #
     ###################################
-
-    def query_on_all_chains(self, schemas: List = None):
-        if not schemas:
-            schemas = [schema for schema in self.get_all_schemas() if schema.startWith('chain')]
+    @staticmethod
+    def grant_select(schemas: List, tables: List, username):
         for schema in schemas:
-            query = f"""
-                select {schema}.smart_contract
-            """
-            self.session.execute(query)
-            self.session.commit()
-            print(f"Finish query on {schema}")
-
-    def grant_select(self, schemas: List, tables: List, username):
-        for schema in schemas:
-            query = f"""
-                GRANT USAGE ON SCHEMA {schema} TO {username};
-            """
-            for table in tables:
-                query += f"""
-                    GRANT SELECT ON {schema}.{table} TO {username};
+            with Session.begin() as session:
+                query = f"""
+                    GRANT USAGE ON SCHEMA {schema} TO {username};
                 """
-            # self.session.execute(query)
-            # self.session.commit()
-            print(query)
-            # logger.info(f"Grant select on {schema}: {tables} for {username}")
+                for table in tables:
+                    query += f"""
+                        GRANT SELECT ON {schema}.{table} TO {username};
+                    """
+                # session.execute(query)
+                logger.info(f"Grant select on {schema}: {tables} for {username}")
+                # print(query)
 
-    def revoke_before_delete(self, schemas: List, tables: List, username):
+    @staticmethod
+    def revoke_before_delete(schemas: List, tables: List, username):
         for schema in schemas:
-            query = ""
-            for table in tables:
+            with Session.begin() as session:
+                query = ""
+                for table in tables:
+                    query += f"""
+                        REVOKE ALL PRIVILEGES ON {schema}.{table} FROM {username};
+                    """
                 query += f"""
-                    REVOKE ALL PRIVILEGES ON {schema}.{table} FROM {username};
+                    REVOKE ALL PRIVILEGES ON SCHEMA {schema} FROM {username};
                 """
-            query += f"""
-                REVOKE ALL PRIVILEGES ON SCHEMA {schema} FROM {username};
-            """
-            self.session.execute(query)
-            self.session.commit()
-            print(query)
-            # logger.info(f"Grant select on {schema}: {tables} for {username}")
+                session.execute(query)
+                print(query)
+                # logger.info(f"Grant select on {schema}: {tables} for {username}")
 
-    def grant_usage_schema(self, schema: str, username: str):
-        q = f"GRANT USAGE ON SCHEMA {schema} TO {username}"
-        self.session.execute(q)
-        self.session.commit()
+    @staticmethod
+    def grant_usage_schema(schema: str, username: str):
+        with Session.begin() as session:
+            q = f"GRANT USAGE ON SCHEMA {schema} TO {username}"
+            session.execute(q)
         return 0
 
 
 if __name__ == '__main__':
     pg = PostgresDB()
-    schemas = ['chain_0x38', 'chain_0x1', 'chain_0xfa', 'chain_0x89',
-               'chain_0xa', 'chain_0xa86a', 'chain_0xa4b1', 'chain_0x2b6653dc']
-    username = 'token_transfer_reader_2'
-    tables = ['token_transfer', 'token_decimals']
-    pg.revoke_before_delete(schemas=schemas, tables=tables, username=username)
+    schemas_ = ['chain_0x38', 'chain_0x1', 'chain_0xfa', 'chain_0x89',
+                'chain_0xa', 'chain_0xa86a', 'chain_0xa4b1', 'chain_0x2b6653dc']
+    username_ = 'token_transfer_reader_2'
+    tables_ = ['token_transfer', 'token_decimals']
+    pg.revoke_before_delete(schemas=schemas_, tables=tables_, username=username_)
